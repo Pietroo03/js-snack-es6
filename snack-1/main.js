36475f7fe13d695dca19e95b18143ceb7e03c096
Pietroo03 <pietro.ponte.03@gmail.com>
2024-10-11 15:27:04 +0200

    create array of objects

---
+++ snack-1/main.js
@@ -0,0 +1,24 @@
+/* Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore. */
+
+const bici = [
+    {
+        nome: 'specialized',
+        peso: '12Kg'
+    },
+    {
+        nome: 'trek',
+        peso: '8Kg'
+    },
+    {
+        nome: 'scott',
+        peso: '12.6Kg'
+    },
+    {
+        nome: 'olmo',
+        peso: '10.3Kg'
+    }
+]
+
+console.log(bici);
