ce236a23885db2c59e4690ebc073f98adec5d477
Pietroo03 <pietro.ponte.03@gmail.com>
2024-10-11 17:35:09 +0200

    add cicle for to create random numbers

---
+++ snack-2/main.js
@@ -0,0 +1,34 @@
+/* Creare un array di oggetti di squadre di calcio.
+Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console */
+
+const teams = [
+    {
+        nome: 'Atletico ma non troppo',
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: 'Real Birra',
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: 'Leiceres',
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: 'Valponte',
+        punti: 0,
+        falli: 0
+    }
+
+]
+
+for (let i = 0; i < teams.length; i++) {
+    teams[i].punti = Math.floor(Math.random() * 100) + 1;
+    teams[i].falli = Math.floor(Math.random() * 50) + 1;
+}
